302c5d19d8a6101991614af6f1dffb00f7cfe6da
nat1103 <coursnathan57@gmail.com>
2024-05-27 17:21:10 +0200

    change port for app flask

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -6,4 +7,6 @@ app = Flask(__name__)
 def index():
     return 'Web App with Python Flask!'
 
-app.run(host='0.0.0.0', port=81)
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 8080))  # Utilisez 8080 comme valeur par défaut si la variable d'environnement PORT n'est pas définie
+    app.run(host='0.0.0.0', port=port)
